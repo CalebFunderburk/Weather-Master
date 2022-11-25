2a1fe72ebd44caad961355dc89a2d9440ebeb477
Caleb <calebfunderburk@icloud.com>
2022-11-25 17:02:00 -0500

    rendering 5 day forecast

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,7 +14,12 @@ const currentTemp = document.getElementById('temp')
 const currentHumid = document.getElementById('humid')
 const currentWind = document.getElementById('wind')
 const currentUv = document.getElementById('uv')
-let fivedayHeader = document.getElementById('fiveday-header')
+const forecastHeader = document.getElementById('forecast-header')
+const forecast1 = document.getElementById('forecast-1')
+const forecast2 = document.getElementById('forecast-2')
+const forecast3 = document.getElementById('forecast-3')
+const forecast4 = document.getElementById('forecast-4')
+const forecast5 = document.getElementById('forecast-5')
 
 // Search history
 let cityArray = []
@@ -29,7 +34,7 @@ const formHandler = (event) => {
         alert('Please enter a city name')
     } else {
         getCurrent(citySearch)
-        // getForecast(citySearch)
+        getForecast(citySearch)
 
         cityArray.push(citySearch)
         localStorage.setItem('citySearch', JSON.stringify(cityArray))
@@ -41,13 +46,14 @@ const clickHandler = (event) => {
 
     const cityHistory = event.currentTarget.textContent
     getCurrent(cityHistory)
-    // getForecast(cityHistory)
+    getForecast(cityHistory)
 
 }
 
-// API Fetch request for current weather
+// Fetch current weather API data
 const getCurrent = (citySearch) => {
 
+    // API requirements
     const key = '3cc750875e9720f210c3ade16c226b21'
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=${key}&units=imperial`
 
@@ -68,11 +74,10 @@ const getCurrent = (citySearch) => {
 
 }
 
-// cityUv function goes here
-
+// Display current weather data
 const displayCurrent = (data, citySearch) => {
 
-    // Display current weather data
+    // Format & display current weather data
     cityName.innerHTML = data.name
     currentIcon.src = "assets/images/" + data.weather[0].icon + ".png"
     currentIcon.alt = "Weather icon"
@@ -80,22 +85,24 @@ const displayCurrent = (data, citySearch) => {
     currentHumid.innerHTML = data.main.humidity + "%"
     currentWind.innerHTML = data.wind.speed + " MPH"
 
-    //Format & save search to history
+    // Format & save search to history
     const newHistory = document.createElement('li')
     newHistory.className = 'list-group-item'
     newHistory.textContent = citySearch
     newHistory.addEventListener('click', clickHandler)
     historyList.appendChild(newHistory)
 
-    //  Use lat & lon to acquire UV index
+    //  Use lat & lon to acquire current UV index
     const lat = data.coord.lat
     const lon = data.coord.lon
     searchUv(lat, lon, citySearch)
 
 }
 
+// Fetch and display current UV index API data
 const searchUv = (lat, lon, citySearch) => {
 
+    // API requirements
     const key = '3cc750875e9720f210c3ade16c226b21'
     const url = `https://api.openweathermap.org/data/2.5/uvi?q=${citySearch}&appid=${key}&lat=${lat}&lon=${lon}`
 
@@ -115,5 +122,150 @@ const searchUv = (lat, lon, citySearch) => {
         })
 }
 
-// Event listeners
+// Fetch 5 day forecast API data
+const getForecast = (citySearch) => {
+    
+    // API requirements
+    const key = '3cc750875e9720f210c3ade16c226b21'
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${citySearch}&units=imperial&cnt=6&appid=${key}`
+
+    fetch(url)
+    .then((res) => {
+        if (!res.ok) {
+            alert(`Error: ${res.statusText}`)
+        } else {
+            res.json()
+                .then((data) => {
+                    displayForecast(data.list)
+                })
+        }
+    })
+    .catch((err) => {
+        alert('Unable to connect to Open Weather')
+    })
+}
+
+// Display 5 day forecast data
+const displayForecast = (data) => {
+
+    // Header
+    forecastHeader.innerHTML = '5 Day Forecast'
+
+    // Format & display dates
+    const date1 = document.createElement('p')
+    const date1Format = moment().add(1, 'days').format('L')
+    date1.innerHTML = date1Format
+    forecast1.appendChild(date1)
+
+    const date2 = document.createElement('p')
+    const date2Format = moment().add(2, 'days').format('L')
+    date2.innerHTML = date2Format
+    forecast2.appendChild(date2)
+
+    const date3 = document.createElement('p')
+    const date3Format = moment().add(3, 'days').format('L')
+    date3.innerHTML = date3Format
+    forecast3.appendChild(date3)
+
+    const date4 = document.createElement('p')
+    const date4Format = moment().add(4, 'days').format('L')
+    date4.innerHTML = date4Format
+    forecast4.appendChild(date4)
+
+    const date5 = document.createElement('p')
+    const date5Format = moment().add(5, 'days').format('L')
+    date5.innerHTML = date5Format
+    forecast5.appendChild(date5)
+
+    // Format & diplay icons
+    const icon1 = document.createElement('img')
+    icon1.src = `assets/images/${data[0].weather[0].icon}.png`
+    icon1.alt = 'Weather icon'
+    forecast1.appendChild(icon1)
+
+    const icon2 = document.createElement('img')
+    icon2.src = `assets/images/${data[1].weather[0].icon}.png`
+    icon2.alt = 'Weather icon'
+    forecast2.appendChild(icon2)
+
+    const icon3 = document.createElement('img')
+    icon3.src = `assets/images/${data[2].weather[0].icon}.png`
+    icon3.alt = 'Weather icon'
+    forecast3.appendChild(icon3)
+
+    const icon4 = document.createElement('img')
+    icon4.src = `assets/images/${data[3].weather[0].icon}.png`
+    icon4.alt = 'Weather icon'
+    forecast4.appendChild(icon4)
+
+    const icon5 = document.createElement('img')
+    icon5.src = `assets/images/${data[4].weather[0].icon}.png`
+    icon5.alt = 'Weather icon'
+    forecast5.appendChild(icon5)
+
+    // Format & display temperature
+    const temp1 = document.createElement('p')
+    temp1.innerHTML = `${data[0].main.temp}  °F`
+    forecast1.appendChild(temp1)
+
+    const temp2 = document.createElement('p')
+    temp2.innerHTML = `${data[1].main.temp}  °F`
+    forecast2.appendChild(temp2)
+
+    const temp3 = document.createElement('p')
+    temp3.innerHTML = `${data[2].main.temp}  °F`
+    forecast3.appendChild(temp3)
+
+    const temp4 = document.createElement('p')
+    temp4.innerHTML = `${data[3].main.temp}  °F`
+    forecast4.appendChild(temp4)
+
+    const temp5 = document.createElement('p')
+    temp5.innerHTML = `${data[4].main.temp}  °F`
+    forecast5.appendChild(temp5)
+
+    // Format & display wind speed
+    const wind1 = document.createElement('p')
+    wind1.innerHTML = `${data[0].wind.speed} MPH`
+    forecast1.appendChild(wind1)
+
+    const wind2 = document.createElement('p')
+    wind2.innerHTML = `${data[1].wind.speed} MPH`
+    forecast2.appendChild(wind2)
+
+    const wind3 = document.createElement('p')
+    wind3.innerHTML = `${data[2].wind.speed} MPH`
+    forecast3.appendChild(wind3)
+
+    const wind4 = document.createElement('p')
+    wind4.innerHTML = `${data[3].wind.speed} MPH`
+    forecast4.appendChild(wind4)
+
+    const wind5 = document.createElement('p')
+    wind5.innerHTML = `${data[4].wind.speed} MPH`
+    forecast5.appendChild(wind5)
+
+    // Format & display humidity
+    const humid1 = document.createElement('p')
+    humid1.innerHTML = `${data[0].main.humidity}%`
+    forecast1.appendChild(humid1)
+
+    const humid2 = document.createElement('p')
+    humid2.innerHTML = `${data[1].main.humidity}%`
+    forecast2.appendChild(humid2)
+
+    const humid3 = document.createElement('p')
+    humid3.innerHTML = `${data[2].main.humidity}%`
+    forecast3.appendChild(humid3)
+
+    const humid4 = document.createElement('p')
+    humid4.innerHTML = `${data[3].main.humidity}%`
+    forecast4.appendChild(humid4)
+
+    const humid5 = document.createElement('p')
+    humid5.innerHTML = `${data[4].main.humidity}%`
+    forecast5.appendChild(humid5)
+}
+
+// Event listener
 searchBtn.addEventListener('click', formHandler)
